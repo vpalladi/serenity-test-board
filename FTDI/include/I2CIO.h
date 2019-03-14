51f578dbe4f8e47e92f7cc79cc84ec118b80c8e6
David Monk <dm2614@ic.ac.uk>
2019-03-14 11:48:35 +0000

    Added headder file for i2ctest

--- FTDI/include/I2CIO.h
+++ FTDI/include/I2CIO.h
@@ -3,12 +3,6 @@
 /* C */
 #include <stdio.h>
 #include <stdlib.h>
-#include <getopt.h>
-#include <string.h>
-#include <math.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 
 /* mpasse */
 #include <mpsse.h>
---
+++ FTDI/include/i2ctest.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/* C */
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+#include <math.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+/* mpasse */
+#include <mpsse.h>
+
+#include "consts.h"
+#include "I2CIO.h"
